e1419ea6ea21d45f287652d64f0b63302f8a19b7
RJRowland93 <r.rowland93@gmail.com>
2016-11-15 15:30:15 -0600

    gifs now clear out

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -26,6 +26,8 @@ $(document).ready(function() {
 
 	$(document).on("click", ".meme", function() {
 
+		$("#images").empty();
+
 	var search = $(this).attr("data-name");
 	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=dc6zaTOxFJmzC&limit=10";
 
